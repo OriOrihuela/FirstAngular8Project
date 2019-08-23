218f3b75fb82f06a66a748c952816af3cdb91e20
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-08-23 10:40:28 +0200

    Basic ngIf directives added; two-directioned event binding added.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,31 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.scss"]
 })
 export class ServersComponent implements OnInit {
   /**
    * PROPERTIES OF THE COMPONENT
    */
-  allowNewServer:boolean = false;
-  serverCreationStatus:string = "No server was created!"
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = "No server was created!";
+  serverName: any = "TestServer";
+  serverCreated: boolean = false;
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000)
-   }
-
-  ngOnInit() {
+    }, 2000);
   }
+
+  ngOnInit() {}
 
   /**
    * MAIN METHODS
    */
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      "Server was created! Name is " + this.serverName;
   }
 
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
