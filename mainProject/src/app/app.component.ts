d347ab6c54ff34b6b1fbdadc6ed4584ddf666985
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-08-28 12:51:23 +0200

    Navigation to recipes and shopping list added to the header.

--- mainProject/src/app/app.component.ts
+++ mainProject/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'mainProject';
+  /**
+   * PROPERTIES
+   */
+  title: string = "mainProject";
+  loadedFeature: string = "recipe";
+
+  /**
+   * BEHAVIOURS
+   */
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- mainProject/src/app/header/header.component.ts
+++ mainProject/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  /**
+   * PROPERTIES
+   */
+  @Output("ftSelected") featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * BEHAVIOURS
+   */
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
