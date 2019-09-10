f5526ac1cd35f4943534559640ff967607b86363
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-09-10 10:45:38 +0200

    Form assingment finished.

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  /**
+   * PROPERTIES
+   */
+  subscriptions: string[] = ["Basic", "Advanced", "Pro"];
+  selectedSubscription: string = "Advanced";
+  @ViewChild("signUpForm", {static: false}) sgnForm: NgForm;
 
+  /**
+   * BEHAVIOURS
+   */
+  onSubmit() {
+    console.log(this.sgnForm.value);
+  }
 }
