2b058580a1548ff237dacb6fd39253122c26be76
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-09-12 12:44:11 +0200

    Recipe-edit component functionality added.

--- mainProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ mainProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { FormGroup, FormControl } from "@angular/forms";
+import { RecipesService } from "../services/recipes.service";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.scss']
+  selector: "app-recipe-edit",
+  templateUrl: "./recipe-edit.component.html",
+  styleUrls: ["./recipe-edit.component.scss"]
 })
 export class RecipeEditComponent implements OnInit {
   /**
    *  PROPERTIES
    */
-  id: Number;
+  id: number;
   editMode: boolean = false;
-  
+  recipeForm: FormGroup;
+
   /**
    * CONSTRUCTOR
    */
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
+
+  /**
+   * GETTERS
+   */
+  getId() {
+    return this.id;
+  }
+
+  getEditMode() {
+    return this.editMode;
+  }
+
+  getRecipeForm() {
+    return this.recipeForm;
+  }
+
+  getRecipesService() {
+    return this.recipesService;
+  }
+
+  getRoute() {
+    return this.route;
+  }
 
   /**
    * BEHAVIOURS
@@ -25,7 +54,29 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params["id"];
       this.editMode = params["id"] != null;
+      this.initForm();
     });
   }
 
+  onSubmit() {
+    console.log(this.getRecipeForm());
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+
+    if (this.getEditMode()) {
+      const recipe = this.getRecipesService().getRecipe(this.getId());
+      recipeName = recipe.getName();
+      recipeImagePath = recipe.getImagePath();
+      recipeDescription = recipe.getDescription();
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription)
+    });
+  }
 }
