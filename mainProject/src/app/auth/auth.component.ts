acb5c83041a7f9fb495d88c74a54fa815ef1b0a2
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-09-25 12:02:19 +0200

    Alert box added when user introduces wrong data in the auth form (imperatively).

--- mainProject/src/app/auth/auth.component.ts
+++ mainProject/src/app/auth/auth.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ComponentFactoryResolver,
+  ViewChild,
+  OnDestroy
+} from "@angular/core";
 import { FormGroup, FormControl, Validators, NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Observable, Subscription } from "rxjs";
+import { Router } from "@angular/router";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
   styleUrls: ["./auth.component.scss"]
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   /**
    * PROPERTIES
    */
@@ -17,11 +25,18 @@ export class AuthComponent implements OnInit {
   isLoading: boolean = false;
   error: string = null;
   form: FormGroup;
+  @ViewChild(PlaceholderDirective, { static: false })
+  alertHost: PlaceholderDirective;
+  private closeSub: Subscription;
 
   /**
    * CONSTRUCTOR
    */
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
   /**
    * BEHAVIOURS
@@ -31,6 +46,12 @@ export class AuthComponent implements OnInit {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required)
     });
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 
   onSwitchMode() {
@@ -45,7 +66,9 @@ export class AuthComponent implements OnInit {
     const password = form.value.password;
     let authObservable: Observable<AuthResponseData>;
     this.isLoading = true;
-    this.isLoginMode ? authObservable = this.authService.login(email, password) : authObservable = this.authService.signUp(email, password);
+    this.isLoginMode
+      ? (authObservable = this.authService.login(email, password))
+      : (authObservable = this.authService.signUp(email, password));
     authObservable.subscribe(
       resData => {
         this.isLoading = false;
@@ -53,6 +76,7 @@ export class AuthComponent implements OnInit {
       },
       errorMessage => {
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     );
@@ -61,5 +85,19 @@ export class AuthComponent implements OnInit {
 
   onHandleError() {
     this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    const alertComponent = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    const componentRef = hostViewContainerRef.createComponent(alertComponent);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
