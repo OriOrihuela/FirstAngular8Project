eae0027363134a8d05a4f59223bd626f50c989f3
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-09-26 09:46:52 +0200

    Lazy-loading added.

--- mainProject/src/app/app-routing.module.ts
+++ mainProject/src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   {
     path: "shopping-list",
     component: ShoppingListComponent
+  },
+  {
+    path: "recipes",
+    loadChildren: () =>
+      import("./recipes/recipes.module").then(m => m.RecipesModule)
+  },
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- mainProject/src/app/app.module.ts
+++ mainProject/src/app/app.module.ts
@@ -7,11 +7,9 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
-import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { SharedModule } from "./shared/shared.module";
-import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
@@ -25,8 +23,6 @@ import { AuthModule } from "./auth/auth.module";
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
-    AuthModule,
     SharedModule
   ],
   providers: [
